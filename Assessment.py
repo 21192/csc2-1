1eaf660216ed30aad227ff5278da4736c793f4d8
Salde <21192@fraser.school.nz>
2024-04-03 09:49:46 +1300

    test

--- Assessment.py
+++ Assessment.py
@@ -1,1 +1,28 @@
-A = input("Commit")
+def History():
+    print(Area, Perimeter)
+
+History_List 
+
+
+
+
+while True:
+    try:
+        Shape = input("Shape:\n(A) Triangle\n(B) Square\n(C) Rectangle\n(D) Circle\n")
+        if Shape == "A" or Shape == "a":
+            print("A")
+            break
+        elif Shape == "B" or Shape == "b":
+            print("B")
+            break
+        elif Shape == "C" or Shape == "c":
+            print("C")
+            break
+        elif Shape == "D" or Shape == "d":
+            print("D") 
+            break
+    except ValueError:
+            print("**Error**")
+            continue
+Area = input("{}*{} =".format())
+Perimeter = input({}+{})
