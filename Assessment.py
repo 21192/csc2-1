224d4baaa68d116e0292d3faa1bb3a816aaedcd1
Salde <21192@fraser.school.nz>
2024-04-03 10:27:43 +1300

    3/04/2024

--- Assessment.py
+++ Assessment.py
@@ -1,28 +1,53 @@
 def History():
     print(Area, Perimeter)
 
-History_List 
 
 
+History_list = []
+Equations = 0
 
 
 while True:
     try:
-        Shape = input("Shape:\n(A) Triangle\n(B) Square\n(C) Rectangle\n(D) Circle\n")
-        if Shape == "A" or Shape == "a":
-            print("A")
-            break
-        elif Shape == "B" or Shape == "b":
-            print("B")
-            break
-        elif Shape == "C" or Shape == "c":
-            print("C")
-            break
-        elif Shape == "D" or Shape == "d":
-            print("D") 
-            break
+        AandP = input ("(A) Area\n(B) Perimeter\n")
+        if AandP == "A" or AandP == "a":
+    
+            AShape = input("Shape:\n(A) Triangle\n(B) Square\n(C) Rectangle\n(D) Circle\n")
+            if AShape == "A" or AShape == "a":
+                THeight = int(input("Height: "))
+                TBase = int(input("Base: "))
+                print(THeight * TBase /2) 
+                break
+            elif AShape == "B" or AShape == "b":
+                print("B")
+                break
+            elif AShape == "C" or AShape == "c":
+                print("C")
+                break
+            elif AShape == "D" or AShape == "d":
+                print("D") 
+                break
+        
+        elif AandP == "B" or AandP == "b":        
+            PShape = input("Shape:\n(A) Triangle\n(B) Square\n(C) Rectangle\n(D) Circle\n")
+            if PShape == "A" or PShape == "a":
+                T1 = int(input("Size: "))
+                T2 = int(input("Size: "))
+                T3 = int(input("Size: "))
+                print(T1 + T2 + T3)
+                break
+            elif PShape == "B" or PShape == "b":
+                print("B")
+                break
+            elif PShape == "C" or PShape == "c":
+                print("C")
+                break
+            elif PShape == "D" or PShape == "d":
+                print("D")
+                break
     except ValueError:
-            print("**Error**")
-            continue
+        print("**Error**")
+        continue
+
 Area = input("{}*{} =".format())
 Perimeter = input({}+{})
