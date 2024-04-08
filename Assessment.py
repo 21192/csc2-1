7ba7d367eadfe9a97fbcb3de882df984c0202323
Salde <21192@fraser.school.nz>
2024-04-08 14:51:23 +1200

    8/04/2024

--- Assessment.py
+++ Assessment.py
@@ -1,8 +1,10 @@
-def History():
-    print(Area, Perimeter)
+import math
+def myhistory():
+    for x in Equation:
+      print(UserHistory)  
 
-
-π = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+UserHistory = ("UH")
+Equation = 0
 
     
 History_list = []
@@ -11,28 +13,28 @@ Equations = 0
 
 while True:
     try:
-        AandP = input ("(A) Area\n(B) Perimeter\n")
+        AandP = input ("(A) Area\n(B) Perimeter\n(C) HINT\n")
         if AandP == "A" or AandP == "a":
     
             AShape = input("Shape:\n(A) Triangle\n(B) Square\n(C) Rectangle\n(D) Circle\n")
             if AShape == "A" or AShape == "a":
                 THeight = int(input("Height: "))
                 TBase = int(input("Base: "))
-                print(THeight * TBase /2) 
+                print("The Answer is: "+str(THeight * TBase /2)) 
                 break
 
             elif AShape == "B" or AShape == "b":
                 SSize = int(input("Square Size: "))
-                print(SSize * SSize)
+                print("The Answer is: "+str(SSize * SSize))
                 break
             elif AShape == "C" or AShape == "c":
                 RLength = int(input("Length: "))
                 RWidth = int(input("Width: "))
-                print(RLength * RWidth)
+                print("The Answer is: "+str(RLength * RWidth))
                 break
             elif AShape == "D" or AShape == "d":
                 CRadius = int(input("Radius: "))
-                print(CRadius^2 * π )
+                print("The Answer is: "+str(CRadius^2 * math.pi ))
                 break
         
         elif AandP == "B" or AandP == "b":        
@@ -41,24 +43,23 @@ while True:
                 T1 = int(input("Size: "))
                 T2 = int(input("Size: "))
                 T3 = int(input("Size: "))
-                print(T1 + T2 + T3)
+                print("The Answer is: "+str(T1 + T2 + T3))
                 break
             elif PShape == "B" or PShape == "b":
                 SPeri = int(input("Square Size: "))
-                print(SPeri * 4)
+                print("The Answer is: "+str(SPeri * 4))
                 break
             elif PShape == "C" or PShape == "c":
                 R1 = int(input("Length: "))
                 R2 = int(input("Width: "))
-                print(R1 + R1 + R2 + R2)
+                print("The Answer is: "+str(R1 + R1 + R2 + R2))
                 break
             elif PShape == "D" or PShape == "d":
                 C1 = int(input("Radius: "))
-                print(2 * π * C1)
+                print("The Answer is: "+str(2 * math.pi * C1))
                 break
+        elif AandP == "C" or AandP == "c":
+            print("HINT")
     except ValueError:
-        print("**Error**")
+        print("Pick either (A) (B) or (C)")
         continue
-
-Area = input("{}*{} =".format())
-Perimeter = input({}+{})
